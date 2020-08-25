9808b1d253839708f303dca55527c8d8e553481c
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-25 21:08:46 +0900

    duck-typing

---
+++ pythonic/object&class/duck_typing.py
@@ -0,0 +1,66 @@
+# 덕타이핑
+
+class Person(object):
+    def __init__(self, age=1):
+        self.age = age
+    def drive(self):
+        if self.age >= 18:
+            print('ok')
+        else:
+            raise Exception('No drive')
+
+class Baby(Person):
+    def __init__(self, age=1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+class Adult(Person):
+    def __init__(self, age=18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+baby = Baby()
+adult = Adult()
+
+class Car(object):
+    def __init__(self, model=None):
+        self.model = model
+    def run(self):
+        print('run')
+    def ride(self, person):
+        person.drive()
+
+car = Car()
+car.ride(adult)
+
+
+class HyundaiCar(Car):
+    def run(self):
+        print('fast')
+
+class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False, passwd='123'):
+        super().__init__(model)
+        self._enable_auto_run = enable_auto_run
+        self.passwd = passwd
+
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        if self.passwd == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
+
+    def run(self):
+        print('super fast')
+
+    def auto_run(self):
+        print('auto run')
