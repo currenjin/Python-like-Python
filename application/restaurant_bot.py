92007358426f0e58858a6eb4103b19056a7f75b2
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-26 23:25:40 +0900

    update_restaurant_bot

--- application/restaurant_bot.py
+++ application/restaurant_bot.py
@@ -2,29 +2,18 @@ import csv
 
 name = input('안녕하세요! 저는 식당추천 봇이에요. 이름이 무엇인가요?\n')
 
-'''
-def in_csv(restaurant):
-    with open('restaurant.csv', 'a') as csv_file:
-        fieldnames = ['Name', 'Count']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerow({'Name': restaurant, 'Count': 1})
-
-def out_csv():
-    with open('restaurant.csv', 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            print(row['Name'])
-
-in_csv('Apple Restaurant')
-out_csv()
-'''
-
 with open('restaurant.csv', 'r') as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
-        answer = input('이 레스토랑은 어떤가요? {}, [YES/NO]\n'.format(row['Name']))
-        if answer == 'NO':
+        answer = input('이 레스토랑은 어떤가요? ({}), [YES/NO]\n'.format(row['Name']))
+        if answer == 'YES':
             break
 
 restaurant = input('{}님, 좋아하는 레스토랑의 이름은 무엇인가요?\n'.format(name))
+
+with open('restaurant.csv', 'a') as csv_file:
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writerow({'Name': restaurant, 'Count': 1})
+
+print('감사합니다. {}님, 좋은 하루 보내세요!'.format(name))
