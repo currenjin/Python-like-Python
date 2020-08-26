9b35b70bba8e3f315ec483235a14378d4d77dfc0
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-26 15:08:44 +0900

    subprocess

---
+++ pythonic/file/subprocess.py
@@ -0,0 +1,9 @@
+# subprocess 사용해 터미널 명령어를 사용해보자.
+
+import subprocess
+
+# subprocess.run(['ls', '-al'])
+# subprocess.run('ls -al | grep test', shell=True)
+
+# cmd = 'dasdasfdsar | rm -rf *'
+# subprocess.run(cmd, shell=True)
