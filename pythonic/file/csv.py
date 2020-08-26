2dd5e856363f6251113fae80d470dbff8db1f874
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-26 13:14:06 +0900

    csv

--- pythonic/file/csv.py
+++ pythonic/file/csv.py
@@ -1,1 +1,15 @@
 # csv파일을 조작하기
+
+import csv
+
+with open('test.csv', 'w') as csv_file:
+    fieldnames = ['Name', 'Count']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'Name': 'A', 'Count': 1})
+    writer.writerow({'Name': 'B', 'Count': 2})
+
+with open('test.csv', 'r') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row['Name'], row['Count'])
